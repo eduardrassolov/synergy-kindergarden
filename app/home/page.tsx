8844a633b3aa8c3b43431fd84b950feb784dd170
eduardrassolov <eduard.rassolov@outlook.com>
2023-12-12 22:31:41 +0200

    added navbar with burger

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,6 +2,41 @@ import React from 'react'
 
 export default function HomePage() {
     return (
-        <div>HomePage</div>
+        <main className='h-[100vh] mx-auto px-4'>
+            <section>
+                <article>
+                    <h1 className='text-center text-xl font-semibold my-2'>Ласкаво просимо до Синегрія</h1>
+
+                    <p>Україномовний центр розвитку для дошкільнят у м.Констанца.</p>
+                    <p>Ми - простір розвитку, безпеки, щасливого дитинства в нових реаліях!</p>
+                </article>
+
+                <div className="flex flex-col mt-5 gap-4">
+                    <article>
+                        <h3>Едьютейнмент</h3>
+                        <p>Навчання розвагою</p>
+                    </article>
+
+                    <article>
+                        <h3>Харчування</h3>
+                        <p>Збалансоване і корисне чотириразове харчування</p>
+                    </article>
+
+                    <article>
+                        <h3>Безпека</h3>
+                        <p>Якісне та безпечне обладнання, відеонагляд, мед. працівник</p>
+                    </article>
+
+                    <article>
+                        <h3>Затишок</h3>
+                        <p>Особлива атмосфера затишку і турботи</p>
+                    </article>
+                </div>
+            </section>
+
+            <section>
+
+            </section>
+        </main>
     )
 }
--- app/ui/components/navbar/NavBar.tsx
+++ app/ui/components/navbar/NavBar.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import NavLink from './NavLink'
 
 const navBarLinks = [
@@ -11,16 +13,26 @@ const navBarLinks = [
         title: "Відвідувачам"
     },
     {
+        href: "/blog",
+        title: "Блог"
+    },
+    {
         href: "/contacts",
         title: "Контакти"
     },
 ]
 
 export default function NavBar() {
+    const [isBurgerOpen, setBurger] = useState<boolean>(false);
+    const handleBurger = () => setBurger(prev => !prev);
+
     return (
         <header>
-            <nav className="sticky flex justify-center py-4">
-                <ul className="flex gap-8 text-xl font-semibold">
+            <nav className="sticky flex justify-between py-4 border-solid border-[1px] px-4 mb-6">
+                <h1 className="text-center flex text-3xl font-semibold">Синегрія</h1>
+                <button onClick={handleBurger} className="sm:hidden sticky z-20 text-3xl">{isBurgerOpen ? "-" : "+"}</button>
+
+                <ul className={`${isBurgerOpen ? "flex" : "hidden"} absolute sm:left-0 sm:flex-row z-10 top-0 right-0 py-10 px-6 h-[100vh] bg-white border-solid border-[1px] flex-col gap-4 text-xl font-semibold`}>
                     {navBarLinks.map((navLink, index) =>
                         <li key={`navBarLink_${index}`}>
                             <NavLink href={navLink.href} text={navLink.title} />
